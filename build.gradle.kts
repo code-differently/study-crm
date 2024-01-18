6726f7a98bcaf8365e8b26e6abad083b8f19fd66
Anthony D. Mays <anthony@morganlatimer.com>
2024-01-18 12:55:03 -0800

    chore: configure initial docker compose build

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import com.avast.gradle.dockercompose.ComposeExtension
+
+plugins {
+    `kotlin-dsl`
+    id("com.avast.gradle.docker-compose") version "0.17.6"
+}
+
+repositories {
+    mavenCentral()
+}
+
+val eventuateCommonImageVersion: String by project
+val eventuateCdcImageVersion: String by project
+val eventuateMessagingKafkaImageVersion: String by project
+
+configure<ComposeExtension> {
+    includeDependencies.set(true)
+    environment.put("EVENTUATE_COMMON_VERSION", eventuateCommonImageVersion)
+    environment.put("EVENTUATE_CDC_VERSION", eventuateCdcImageVersion)
+    environment.put("EVENTUATE_MESSAGING_KAFKA_IMAGE_VERSION", eventuateMessagingKafkaImageVersion)
+
+    createNested("simplecrm").apply {
+        setProjectName(null)
+        environment.putAll(mapOf("TAGS" to "feature-test,local"))
+        useComposeFiles.set(listOf("docker-compose.yaml"))
+        startedServices.set(listOf("zipkin", "zookeeper", "kafka", "contact-service", "cdc-service"))
+    }
+}
+
+val buildAndStartService by tasks.registering(GradleBuild::class) {
+    tasks = listOf("simplecrmComposeUp")
+}
