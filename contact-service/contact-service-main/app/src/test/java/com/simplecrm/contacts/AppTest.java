ace78b2c026eaf668e003a5d982ecac4a7fb3d0a
Anthony D. Mays <anthony@morganlatimer.com>
2024-01-18 00:23:40 -0800

    chore: cleaned up imports

--- contact-service/contact-service-main/app/src/test/java/com/simplecrm/contacts/AppTest.java
+++ contact-service/contact-service-main/app/src/test/java/com/simplecrm/contacts/AppTest.java
@@ -3,11 +3,14 @@
  */
 package com.simplecrm.contacts;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
+    @Test
+    void appHasAGreeting() {
         App classUnderTest = new App();
+        assertNotNull(classUnderTest);
     }
 }
--- contact-service/contact-service-web/lib/src/main/java/com/simplecrm/contacts/web/ContactsController.java
+++ contact-service/contact-service-web/lib/src/main/java/com/simplecrm/contacts/web/ContactsController.java
@@ -2,7 +2,6 @@ package com.simplecrm.contacts.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.http.ResponseEntity;
 
 @RestController
