32811e907fbdd3a4e796567e5181e2e1393d5dd5
Anthony D. Mays <anthony@morganlatimer.com>
2024-02-05 04:56:38 +0000

    chore: linting

--- auth-service/auth-service-web/auth-web-lib/src/test/java/com/codedifferently/studycrm/auth/web/controllers/LoginControllerTest.java
+++ auth-service/auth-service-web/auth-web-lib/src/test/java/com/codedifferently/studycrm/auth/web/controllers/LoginControllerTest.java
@@ -1,6 +1,7 @@
 package com.codedifferently.studycrm.auth.web.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -9,9 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(LoginController.class)
 @AutoConfigureMockMvc
