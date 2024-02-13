deda809305939c4c566da72d8ff2fc0ed8e604a9
Anthony D. Mays <anthony@morganlatimer.com>
2024-02-13 01:52:08 +0000

    chore: adds login redirect to dashboard page.

---
+++ studycrm-web/studycrm-web-app/src/app/dashboard/page.tsx
@@ -0,0 +1,69 @@
+import { auth, signIn } from '@/auth';
+import { Card, Title, Text } from '@tremor/react';
+import { gql } from '@/gql';
+import { gqlClient } from '@/app/api/graphql/gql-client';
+import Search from './search';
+import UsersTable from './table';
+import { redirect } from 'next/navigation';
+
+interface User {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+}
+
+const entitiesQuery = gql(/* GraphQL */ `
+  query Query($type: String!) {
+    entities(type: $type) {
+      id
+      type
+      properties {
+        name
+        type
+        value
+      }
+    }
+  }
+`);
+
+export default async function IndexPage({
+  searchParams
+}: {
+  searchParams: { q: string };
+}) {
+  const session = await auth();
+  if (!session) {
+    redirect('/auth/signin');
+  }
+
+  const result = await gqlClient.query({
+    query: entitiesQuery, 
+    variables: {type: 'contact'}
+  });
+
+  const users = new Array<User>();
+  for (const entity of (result.data?.entities || [])) {
+    const propertyMap = entity.properties.reduce((acc, property) => {
+      acc[property.name] = property.value;
+      return acc;
+    }, {} as Record<string, string>);
+    users.push({
+      id: entity.id,
+      name: propertyMap['first_name'] + ' ' + propertyMap['last_name'],
+      username: propertyMap['email'],
+      email: propertyMap['email'],
+    });
+  }
+
+  return (
+    <main className="p-4 md:p-10 mx-auto max-w-7xl">
+      <Title>Users</Title>
+      <Text>A list of users retrieved from a Postgres database.</Text>
+      <Search />
+      <Card className="mt-6">
+        <UsersTable users={users} />
+      </Card>
+    </main>
+  );
+}
--- studycrm-web/studycrm-web-app/src/app/page.tsx
+++ studycrm-web/studycrm-web-app/src/app/page.tsx
@@ -1,68 +1,14 @@
-import { Card, Title, Text } from '@tremor/react';
-import { gql } from '@/gql';
-import Search from './search';
-import UsersTable from './table';
-import { gqlClient } from './api/graphql/gql-client';
-import { auth, signIn } from '@/auth';
-
-interface User {
-  id: string;
-  name: string;
-  username: string;
-  email: string;
-}
-
-const entitiesQuery = gql(/* GraphQL */ `
-  query Query($type: String!) {
-    entities(type: $type) {
-      id
-      type
-      properties {
-        name
-        type
-        value
-      }
-    }
-  }
-`);
+import { Title, Text } from '@tremor/react';
 
 export default async function IndexPage({
   searchParams
 }: {
   searchParams: { q: string };
 }) {
-  const session = await auth();
-  if (!session) {
-    signIn();
-  }
-
-  const result = await gqlClient.query({
-    query: entitiesQuery, 
-    variables: {type: 'contact'}
-  });
-
-  const users = new Array<User>();
-  for (const entity of (result.data?.entities || [])) {
-    const propertyMap = entity.properties.reduce((acc, property) => {
-      acc[property.name] = property.value;
-      return acc;
-    }, {} as Record<string, string>);
-    users.push({
-      id: entity.id,
-      name: propertyMap['first_name'] + ' ' + propertyMap['last_name'],
-      username: propertyMap['email'],
-      email: propertyMap['email'],
-    });
-  }
-
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      <Title>Users</Title>
-      <Text>A list of users retrieved from a Postgres database.</Text>
-      <Search />
-      <Card className="mt-6">
-        <UsersTable users={users} />
-      </Card>
+      <Title>Hompage</Title>
+      <Text>This is the homepage.</Text>
     </main>
   );
 }
