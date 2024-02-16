f316fce2cda4d55ab35c8760ce16cbe9ea3602fc
Anthony D. Mays <anthony@morganlatimer.com>
2024-02-16 00:38:04 +0000

    chore: removing unneeded stuff.

--- studycrm-web/studycrm-web-app/src/graphql/apollo-wrapper.tsx
+++ studycrm-web/studycrm-web-app/src/graphql/apollo-wrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ApolloClient, ApolloLink,  HttpLink } from "@apollo/client";
+import { ApolloLink,  HttpLink } from "@apollo/client";
 import { ApolloNextAppProvider, NextSSRApolloClient, NextSSRInMemoryCache, SSRMultipartLink } from "@apollo/experimental-nextjs-app-support/ssr";
 
 function makeClient() {
